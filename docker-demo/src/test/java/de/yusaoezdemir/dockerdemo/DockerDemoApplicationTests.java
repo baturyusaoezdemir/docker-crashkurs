ddffe61c575c7f2f78b7cee6feaa6525a6c24c5a
Özdemir, Batur Yusa <Batur-Yusa.Oezdemir@adesso.de>
2024-01-02 15:13:56 +0100

    intial commit

---
+++ docker-demo/src/test/java/de/yusaoezdemir/dockerdemo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.yusaoezdemir.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
